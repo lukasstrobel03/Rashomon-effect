d508575642d6bd42c2954f1029d625b8999cd02d
Lukas Strobel <lukas.strobel1@mailbox.tu-dresden.de>
2026-07-20 16:51:21 +0200

    maybe now fixed

--- rashomon-personalization/src/Experiment/Introduction/iceCreamData.tsx
+++ rashomon-personalization/src/Experiment/Introduction/iceCreamData.tsx
@@ -1,24 +1,16 @@
 import iceCreamData from "./assets/20241104_plot_data_ice_cream.json";
 import {type DashboardData} from "../../DashboardBandit/data.tsx";
 
-export const iceCreamPlotData = Object.fromEntries(
+export const iceCreamPlotData: Record<string, DashboardData> = Object.fromEntries(
     iceCreamData[0].plot_data.map(
         x => [
             x.feat_name,
             {
-                X: x.X,
-                Y: x.Y,
-                Z: x.Z ?? null,
-                type: x.type as "numerical" | "categorical" | "interaction",
-                feat_name: x.feat_name,
-                x_name: x.x_name,
-                y_name: x.y_name,
-                x_ticks: x.x_ticks ?? null,
-                y_ticks: x.y_ticks ?? null,
-                x_labels: x.x_labels ?? null,
-                y_labels: x.y_labels ?? null,
-                smooth: null,
+                ...x,
+                type: (x.type as "numerical" | "categorical" | "interaction"),
+                name: x.feat_name,
+                smooth: x.smooth ?? null,
             }
         ]
     )
-) as unknown as Record<string, DashboardData>
+)
