a7abc4ab993cf62ad8276f57f4222d983ad3b0b3
Lukas Strobel <lukas.strobel1@mailbox.tu-dresden.de>
2026-07-23 12:27:31 +0200

    change group to userGroup

--- rashomon-personalization/functions/analytics.ts
+++ rashomon-personalization/functions/analytics.ts
@@ -8,7 +8,7 @@ export const onRequest : PagesFunction<Env>  = async ({request, env}): Promise<R
     try {
         const event: AnalyticsEvent = await request.json();
         const result = await env.AnalyticsStore.prepare(
-            "INSERT INTO AnalyticsEvents (timestamp, userId, experimentTag, commitHash, type, payload, group) VALUES (?, ?, ?, ?, ?, ?, ?)"
+            "INSERT INTO AnalyticsEvents (timestamp, userId, experimentTag, commitHash, type, payload, userGroup) VALUES (?, ?, ?, ?, ?, ?, ?)"
         ).bind(Date.now(), event.userId, event.experimentTag, event.commitHash, event.type, JSON.stringify(event.payload), event.group).run();
 
         return new Response("Analytics event inserted successfully!", { status: 201 });
